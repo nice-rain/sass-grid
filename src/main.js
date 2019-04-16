3c00bb25cda1ea2befd0cc23a367375c1b9dcb88
Walker Twyman <walker.twyman@bottlerocketstudios.com>
2019-04-16 10:54:53 -0500

    Implemented media query generation

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ export default function MainContent(props)
     return( <main>
                 <h1>Test Grid</h1>
                 <div className="container">
-                This is a test.
+                <div className="item"></div>
+                <div className="item"></div>
+                <div className="item"></div>
+                <div className="item"></div>
+                <div className="item"></div>
+                <div className="item"></div>
                 </div>
             </main>);   
 }
