795c93ae3f47bd94e050f0c5beabb18af536ca78
Walker Twyman <walk12288@gmail.com>
2020-08-06 10:59:26 -0500

    update node-sass to support latest node version

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
+const generateItems = numItems =>{
+    const items = [];
+
+    for(let i = 0; i < numItems; i++)
+    {
+        items.push(
+            <div className="item" key={`item_${i}`}>
+                Item {i+1}
+            </div>
+        )
+    }
+
+    return items;
+}
+
 export default function MainContent(props)
 {
     return( <main>
                 <h1>Test Grid</h1>
                 <div className="container">
-                <div className="item"></div>
-                <div className="item"></div>
-                <div className="item"></div>
-                <div className="item"></div>
-                <div className="item"></div>
-                <div className="item"></div>
+                    {generateItems(6)}
                 </div>
             </main>);   
 }
